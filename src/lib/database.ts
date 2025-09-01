fbbac6b3b495ac6fd77143ae99bc236a62291bc6
Raybrilliant <r.f.brilliansyach@gmail.com>
2025-09-01 19:48:30 +0700

    github action deploy

--- src/lib/database.ts
+++ src/lib/database.ts
@@ -1,14 +1,14 @@
 import PocketBase from "pocketbase";
 
-const url =
-  import.meta.env.PUBLIC_SITE_API ??
-  (typeof process !== "undefined" ? process.env.PUBLIC_SITE_API : undefined);
+// const url =
+//   import.meta.env.PUBLIC_SITE_API ??
+//   (typeof process !== "undefined" ? process.env.PUBLIC_SITE_API : undefined);
 
-if (!url) {
-  throw new Error("PUBLIC_SITE_API is not defined");
-}
+// if (!url) {
+//   throw new Error("PUBLIC_SITE_API is not defined");
+// }
 
-const pb = new PocketBase(url);
+const pb = new PocketBase(process.env.PUBLIC_SITE_API as string);
 
 export default pb;
 
