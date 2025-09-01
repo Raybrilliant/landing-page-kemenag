45ff82933c07599f484397e74856a04d43598f92
Raybrilliant <r.f.brilliansyach@gmail.com>
2025-09-01 19:52:42 +0700

    github action deploy

--- src/lib/database.ts
+++ src/lib/database.ts
@@ -1,14 +1,15 @@
 import PocketBase from "pocketbase";
 
-// const url =
-//   import.meta.env.PUBLIC_SITE_API ??
-//   (typeof process !== "undefined" ? process.env.PUBLIC_SITE_API : undefined);
+const url =
+  import.meta.env.PUBLIC_SITE_API ??
+  (typeof process !== "undefined"
+    ? process.env.PUBLIC_SITE_API || process.env.SITE_API
+    : undefined);
 
-// if (!url) {
-//   throw new Error("PUBLIC_SITE_API is not defined");
-// }
+if (!url) {
+  throw new Error("PUBLIC_SITE_API atau SITE_API tidak ditemukan");
+}
 
-const pb = new PocketBase(process.env.PUBLIC_SITE_API as string);
+const pb = new PocketBase(url);
 
 export default pb;
-
