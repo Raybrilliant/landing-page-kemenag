f6d27b7bf51f5b5edd3f6cbdc52f52cc1d9b5bf6
Raybrilliant <r.f.brilliansyach@gmail.com>
2025-09-01 19:04:14 +0700

    github action deploy

--- src/components/Berita.tsx
+++ src/components/Berita.tsx
@@ -1,5 +1,7 @@
 
 export const Berita = ({ title, slug, date, image }: { title: string; slug: string; date: string; image: string }) => {
+    console.log(image);
+    
     return (
         <div className="rounded-md bg-white overflow-hidden border hover:shadow-xl duration-300 grid grid-cols-2 cursor-pointer min-h-52 max-sm:min-h-40 max-sm:min-w-full">
             <img src={image} alt="berita" className="h-full object-cover object-center "/>
--- src/lib/database.ts
+++ src/lib/database.ts
@@ -1,3 +1,3 @@
 import PocketBase from "pocketbase";
-const pb = new PocketBase("http://api-kemenag-kota-probolinggo-pocketbase-103-163-139-55.traefik.me/");
+const pb = new PocketBase(import.meta.env.PUBLIC_SITE_API as string);
 export default pb;
