5a40f56a9858f39ebfe1ac3e08592262b702e515
Raybrilliant <r.f.brilliansyach@gmail.com>
2025-09-19 08:48:02 +0700

    liitle improvement

--- src/components/LayananDetail.tsx
+++ src/components/LayananDetail.tsx
@@ -13,6 +13,7 @@ interface Layanan {
     id: string;
     name: string;
     description: string;
+    external_link: string;
 }
 
 export const LayananDetail = ({id, className}: any) => {
@@ -51,7 +52,7 @@ export const LayananDetail = ({id, className}: any) => {
                     <BlurFade delay={0.12 * index} direction="left" inView key={item.id}>
                             <div className="rounded-md border hover:shadow-xl hover:bg-white transition duration-300 cursor-pointer">
                                 <MagicCard gradientColor={"#D9D9D955"} gradientSize={200} className="p-0">
-                                    <a href={`/layanan/sub-layanan/${item.id}`}>
+                                    <a href={item.external_link || `/layanan/sub-layanan/${item.id}`} target={item.external_link ? '_blank' : '_self'}>
                                     <div className="text-center space-y-3 p-5">
                                         <div className={`${randomBackground[Math.floor(Math.random() * randomBackground.length)]} w-16 max-sm:w-12 h-16 max-sm:h-12 rounded mx-auto flex justify-center items-center`}>
                                             <IconWrapper IconComponentName={randomIcon[Math.floor(Math.random() * randomIcon.length)]} className="w-8 max-sm:w-6 h-8 max-sm:h-6 text-gray-800" />
